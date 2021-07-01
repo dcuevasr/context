0c3c604cdf0045626531126d075f378f97b01c6e
Dario <dario.cuevas_rivera@tu-dresden.de>
2021-07-01 09:31:25 +0200

    First commit of paper

---
+++ adaptation/code/figures.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# .adaptation/code/figures.py
+
+"""Figures for the context inference motor adaptation paper. The default
+parameter --fignum-- for each function defined here determines which figure it
+is in the paper. Those that do not have one are not paper figures, but
+auxiliary functions."""
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+import simulations as sims
+import model
+import task_hold_hand as thh
+import pars
+
+
