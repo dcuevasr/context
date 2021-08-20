758192f9347e2df98ae613c6b48497fb3c6a39ee
Dario <dario.cuevas_rivera@tu-dresden.de>
2021-08-20 11:29:43 +0200

    New version of figure 4 with error bars

--- adaptation/code/figures.py
+++ adaptation/code/figures.py
@@ -21,64 +21,87 @@ import pars
 FIGURE_FOLDER = '../article/figures/'
 
 
-def oh_2019(fignum=1, show=True):
-    """Reproduces the results from Oh_Minimizing_2019 and plots them
-    in a format similar to that of their figure 4.
+def oh_2019_kim_2015(fignum=1, show=True):
+    """Reproduces the results from Oh_Minimizing_2019 and kim_neural_2015 and plots
+    them in a format similar to their plots.
 
     An empty subplot is created to manually add the figure from their
     paper.
 
     """
-    fig, axes = plt.subplots(2, 2, clear=True, num=fignum,
-                             sharex=True, sharey=True)
-    task_20, task_10, agent_pars = sims.oh_2019(plot=False)
+    fig, axes = plt.subplots(3, 2, clear=True, num=fignum)
 
     # a)
+    task_kim, agent_pars = sims.kim_2015(plot=False)
+    agent_kim = model.LRMeanSD(**agent_pars)
+    pandata, pandagent, _ = thh.run(agent_kim, pars=task_kim)
+    pandota_kim = thh.join_pandas(pandata, pandagent)
+    contexts = pandota_kim['ix_context'].values
+    contexts[contexts == pars.CLAMP_INDEX] = 0
+    contexts[contexts == 1] = -40
+    contexts[contexts == 2] = 40
+    axes[0, 0].plot(contexts, color='black')
+    axes[0, 1].plot(contexts, color='black')
+    axes[0, 0].plot(-pandota_kim['pos(t)'] - pandota_kim['action'])
+    axes[0, 0].set_yticks([-40, 0, 40])
+    axes[0, 0].set_ylabel('Adaptation')
+    axes[0, 0].set_title('Simulations')
+    axes[0, 1].set_title('Experimental data')
+    xlim = axes[0, 0].get_xlim()
+    ylim = axes[0, 0].get_ylim()
+    axes[0, 1].set_xlim(xlim)
+    axes[0, 1].set_ylim(ylim)
+    # b)
+    task_20, task_10, agent_pars = sims.oh_2019(plot=False)
+
     agent = model.LRMeanSD(**agent_pars)
     agent.all_learn = True
     pandata, pandagent, _ = thh.run(agent, pars=task_20)
     pandota_20 = thh.join_pandas(pandata, pandagent)
-    axes[0, 0].plot(-pandota_20['pos(t)'] - pandota_20['action'])
     contexts = pandota_20['ix_context'].values
     contexts[contexts == pars.CLAMP_INDEX] = 0
     contexts[contexts == 1] = 20
     contexts_20 = contexts
-    axes[0, 0].plot(contexts, color='black')
-    axes[0, 0].set_ylabel('Adaptation')
-    axes[0, 0].set_title('Simulations')
-    axes[0, 0].text(x=-0.1, y=1, s='A',
-                    transform=axes[0, 0].transAxes,
-                    fontdict={'size': 14})
+    axes[1, 1].plot(contexts_20, color='black')
+    axes[1, 0].plot(-pandota_20['pos(t)'] - pandota_20['action'])
+
+    axes[1, 0].plot(contexts, color='black')
+    axes[1, 0].set_ylabel('Adaptation')
+    xlim = axes[1, 0].get_xlim()
+    ylim = axes[1, 0].get_ylim()
+    axes[1, 1].set_xlim(xlim)
+    axes[1, 1].set_ylim(ylim)
 
     # c)
     agent = model.LRMeanSD(**agent_pars)
     agent.all_learn = True
     pandata, pandagent, _ = thh.run(agent, pars=task_10)
     pandota_10 = thh.join_pandas(pandata, pandagent)
-    axes[1, 0].plot(-pandota_10['pos(t)'] - pandota_10['action'])
     contexts = pandota_10['ix_context'].values
     contexts[contexts == pars.CLAMP_INDEX] = 0
     contexts[contexts == 1] = 10
     contexts_10 = contexts
-    axes[1, 0].plot(contexts, color='black')
-    axes[1, 0].set_xlabel('Trials')
-    axes[1, 0].set_ylabel('Adaptation')
-    axes[1, 0].text(x=-0.1, y=1, s='C',
+    axes[2, 0].plot(contexts_10, color='black')
+    axes[2, 1].plot(contexts_10, color='black')
+    axes[2, 0].plot(-pandota_10['pos(t)'] - pandota_10['action'])
+    axes[2, 0].set_xlabel('Trials')
+    axes[2, 0].set_ylabel('Adaptation')
+    xlim = axes[2, 0].get_xlim()
+    ylim = axes[2, 0].get_ylim()
+    axes[2, 1].set_xlim(xlim)
+    axes[2, 1].set_ylim(ylim)
+
+    axes[2, 1].set_xlabel('Trials')
+
+    # Subplot levels
+    axes[0, 0].text(x=-0.1, y=1, s='A',
+                    transform=axes[0, 0].transAxes,
+                    fontdict={'size': 14})
+    axes[1, 0].text(x=-0.1, y=1, s='B',
                     transform=axes[1, 0].transAxes,
                     fontdict={'size': 14})
-
-    # b)
-    axes[0, 1].set_title('Data from Oh et al (2019)')
-    axes[0, 1].plot(contexts_20, color='black')
-    axes[0, 1].text(x=-0.1, y=1, s='B',
-                    transform=axes[0, 1].transAxes,
-                    fontdict={'size': 14})
-
-    # d)
-    axes[1, 1].set_xlabel('Trials')
-    axes[1, 1].plot(contexts_10, color='black')
-    axes[1, 1].text(x=-0.1, y=1, s='D',
-                    transform=axes[1, 1].transAxes,
+    axes[2, 0].text(x=-0.1, y=1, s='C',
+                    transform=axes[2, 0].transAxes,
                     fontdict={'size': 14})
 
     plt.savefig(FIGURE_FOLDER + 'figure_{}.png'.format(fignum), dpi=600)
@@ -158,34 +181,17 @@ def herzfeld_2014(fignum=2, show=True):
     sensitive_panda['bin_error'] = pd.cut(sensitive_panda['error'],
                                           bins=bins,
                                           labels=bins[:-1])
-    # sns.lineplot(data=sensitive_panda, x='bin_error', y='sensi',
-    #              hue='z_label', palette=colors, ax=axes[2])
     trial_bins = np.linspace(0, num_trials, 7, endpoint=True)
     sensitive_panda['bin_trial'] = pd.cut(sensitive_panda['trial'],
                                           bins=trial_bins,
                                           labels=trial_bins[:-1])
     sns.lineplot(data=sensitive_panda, y='sensi', x='bin_trial',
                  hue='z_label', palette=colors, ax=axes[1])
-    # xlim = np.array((-30, 30))
-    # ylim = np.array((-0.15, 0.15))
-    # axes[1].plot(xlim, [0, 0], color='black', linestyle='dashed',
-    #              alpha=0.2)
-    # axes[1].plot([0, 0], ylim, color='black', linestyle='dashed',
-    #              alpha=0.2)
-    # sns.scatterplot(data=sensitive_panda, x='error',
-    #                 y='sensi', hue='z_label', palette=colors,
-    #                 ax=axes[1])
-    # axes[1].scatter(error_low, error_sensitivity_low, marker='+',
-    #                 label='z=0.9', alpha=0.2, color=colors['low'])
-    # axes[1].scatter(error_high, error_sensitivity_high, marker='+',
-    #                 label='z=0.5', alpha=0.2, color=colors['high'])
     axes[1].set_xlabel('Trials')
     axes[1].set_ylabel('Sensitivity to error (a.u.)')
     yticks = axes[1].get_yticks()
     axes[1].set_yticks(yticks[[0, -1]])
     axes[1].set_yticklabels([0, ''])
-    # axes[1].set_ylim(ylim)
-    # axes[1].set_xlim(xlim)
     axes[1].get_legend().remove()
     axes[1].set_title('Error sensitivity')
     axes[1].text(x=-0.1, y=1, s='B',
@@ -237,45 +243,52 @@ def davidson_2004(fignum=3, show=True):
         plt.show(block=False)
 
 
-def vaswani_2013(fignum=4, show=True):
+def vaswani_2013(fignum=4, show=True, pandota=None):
     """Reproduces the results from Vaswani_Decay_2013, especifically their
     figures 2a-c.
 
     """
+    reps = 10
+    figsize = (6, 7)
     colors = ['blue', 'green', 'red', 'c']
     context_colors = ['black', 'tab:orange', 'tab:purple', 'tab:brown',
                       'tab:olive']
     mags = gs.GridSpec(3, 4, height_ratios=[0.15, 0.15, 0.7], hspace=0.3)
-    fig = plt.figure(num=fignum, clear=True)
+    fig = plt.figure(num=fignum, clear=True, figsize=figsize)
     axes_ada = [fig.add_subplot(mags[0, idx]) for idx in range(4)]
     axes_con = [fig.add_subplot(mags[1, idx]) for idx in range(4)]
     axes_sum = [fig.add_subplot(mags[2, 0:2]), ]
-    axes_sum.append(fig.add_subplot(mags[2, 2:4], sharex=axes_sum[0],
-                                    sharey=axes_sum[0]))
+    axes_sum.append(fig.add_subplot(mags[2, 2:4], sharex=axes_sum[0]))
+
     tasks, agents = sims.vaswani_2013(plot=False)
 
-    adaptations = []
-    subfigures = 'ABCDEF'
-    # Plot adaptation and context inference
+    all_pandas = []
+    names = ['Group {}'.format(group) for group in [1.1, 1.2, 1.3, 1.4]]
+    named_colors = {name: color for name, color in zip(names, colors)}
     for idx, (task, agent_par) in enumerate(zip(tasks, agents)):
-        agent = model.LRMeanSD(**agent_par)
-        pandata, pandagent, _ = thh.run(agent=agent,
-                                        pars=task)
-        pandota = thh.join_pandas(pandata, pandagent)
-        adaptation = pandota['pos(t)'] + pandota['action']
-        adaptations.append(adaptation)
-        axes_ada[idx].plot(-adaptation, color=colors[idx])
-        axes_ada[idx].plot([0, 400], [0, 0], color='black')
-        axes_ada[idx].text(x=-0.1, y=1, s=subfigures[idx],
-                           transform=axes_ada[idx].transAxes,
-                           fontdict={'size': 14})
+        for _ in range(reps):
+            agent = model.LRMeanSD(**agent_par)
+            pandata, pandagent, _ = thh.run(agent=agent,
+                                            pars=task)
+            c_pandota = thh.join_pandas(pandata, pandagent)
+            c_pandota['group'] = names[idx]
+            all_pandas.append(c_pandota)
+    pandota = pd.concat(all_pandas, axis=0)
+    adaptation = -pandota['pos(t)'] - pandota['action']
+    pandota['adaptation'] = adaptation
+    pandota.loc[pandota['group'] == 'Group 1.4', ['adaptation']] *= -1
+    # Plot adaptation
+    # return pandota
+    for group, color, ax in zip(names, colors, axes_ada):
+        c_pandota = pandota.query('group == @group')
+        sns.lineplot(x='trial', y='adaptation', data=c_pandota,
+                     color=color, ax=ax)
+        ax.set_ylabel('')
 
-        axes_ada[idx].set_ylim((-1.1, 1.1))
-        axes_ada[idx].set_title('Group {}'.format(1 + (idx + 1) * 0.1))
-        con_strings = sorted([column for column in pandota.columns
-                              if (column.startswith('con')
-                                  and not column.startswith('con_'))])
-        real_con = np.array(pandata['ix_context'])
+    # Plot context inference
+    for idx, (group, color, ax) in enumerate(zip(names, colors, axes_con)):
+        c_panda = pandota.query('group == @group')
+        real_con = np.array(c_panda.groupby('trial').mean()['ix_context'])
         con_breaks = np.nonzero(np.diff(real_con))[0] + 1
         con_breaks = np.concatenate([[0], con_breaks, [len(real_con) - 1]])
         cons = np.array([real_con[one_break] for one_break in con_breaks])
@@ -283,14 +296,13 @@ def vaswani_2013(fignum=4, show=True):
         for c_con, n_con, ix_con in zip(con_breaks, con_breaks[1:], cons):
             if ix_con == pars.CLAMP_INDEX:
                 ix_con = -1
-            axes_con[idx].fill_between([c_con, n_con], [0] * 2,
-                                       [1] * 2,
-                                       color=context_colors[ix_con],
-                                       alpha=0.2)
-        if idx != 0:
-            axes_con[idx].set_yticks([])
-            axes_ada[idx].set_yticks([])
-        axes_ada[idx].set_xticks([])
+            ax.fill_between([c_con, n_con], [0] * 2,
+                            [1] * 2,
+                            color=context_colors[ix_con],
+                            alpha=0.2)
+        con_strings = sorted([column for column in c_panda.columns
+                              if (column.startswith('con')
+                                  and not column.startswith('con_'))])
         for ix_con, con in enumerate(con_strings):
             # Little hack to get the colors to match between real and inferred:
             c_con = ix_con
@@ -299,30 +311,51 @@ def vaswani_2013(fignum=4, show=True):
             if idx == 3 and ix_con == 1:
                 c_con = 2
             # End little hack.
-            axes_con[idx].plot(pandota[con], color=context_colors[c_con])
-    sum_trials = np.arange(-25, 75)
-    for idx, adaptation in enumerate(adaptations):
-        signo = -1 if idx != 3 else 1
-        axes_sum[0].plot(sum_trials, signo * adaptation[75:174],
-                         color=colors[idx])
+            sns.lineplot(data=c_panda, x='trial', y=con,
+                         color=context_colors[c_con],
+                         ax=axes_con[idx])
+            # axes_con[idx].plot(c_panda.groupby('trial').mean()[con],
+            #                    color=context_colors[c_con])
+
+    # Summary adaptation:
+    pandota_e = pandota.query('trial >= 75 and trial <= 175')
+    pandota_e.reset_index('trial', inplace=True)
+    pandota_e.loc[:, 'trial'] -= 100
+    sns.lineplot(x='trial', y='adaptation', hue='group',
+                 palette=named_colors, data=pandota_e,
+                 ax=axes_sum[0])
+    axes_sum[0].legend()
     y_range = axes_sum[0].get_ylim()
     axes_sum[0].plot([0, 0], y_range, linestyle='dashed',
                      color='black', alpha=0.3)
     axes_sum[1].plot([0, 0], y_range, linestyle='dashed',
                      color='black', alpha=0.3)
+    for idx in range(4):
+        for ax in [axes_con, axes_ada]:
+            ax[idx].set_xlabel('')
+            ax[idx].set_ylabel('')
+            ax[idx].set_xticks([])
+            ax[idx].set_yticks([])
+        axes_ada[idx].set_title(names[idx])
+        axes_con[idx].set_ylim([0, 1])
+        axes_ada[idx].set_ylim([-0.3, 1.1])
+    axes_con[0].set_xlabel('Trial')
+    axes_ada[0].set_xticks([0, 100])
+    axes_con[0].set_yticks([0, 1])
+    axes_ada[0].set_yticks([0, 1])
     axes_sum[0].set_ylim(y_range)
-    axes_sum[0].set_title('Simulated behavior in error-clamp trials')
-    axes_sum[1].set_title('Data adapted from Vaswani et al. 2013')
+    axes_sum[1].set_ylim(y_range)
+    axes_sum[1].set_yticks([])
     axes_sum[0].set_xlabel('Trials since start of error-clamp')
     axes_sum[1].set_xlabel('Trials since start of error-clamp')
     axes_ada[0].set_ylabel('Ad. index')
     axes_sum[0].set_ylabel('Adaptation index')
     axes_con[0].set_ylabel('p(ctx)')
 
-    axes_sum[0].text(x=-0.1, y=1, s='E',
+    axes_sum[0].text(x=-0.05, y=1, s='E',
                      transform=axes_sum[0].transAxes,
                      fontdict={'size': 14})
-    axes_sum[1].text(x=-0.1, y=1, s='F',
+    axes_sum[1].text(x=-0.05, y=1, s='F',
                      transform=axes_sum[1].transAxes,
                      fontdict={'size': 14})
     plt.savefig(FIGURE_FOLDER + 'figure_{}.png'.format(fignum), dpi=600)
